cbd68c7d78074c89e293e912f656da9bbfaa7e53
John78468 <philantrope1@outlook.com>
2021-12-03 01:48:50 +0100

    add sprite add draw player

--- game.rb
+++ game.rb
@@ -3,19 +3,25 @@ require 'gosu'
 class Game < Gosu::Window
   
   def initialize
-    @WIDTH = 640
-    @HEIGTH = 480
+    @WIDTH = 1640
+    @HEIGTH = 1480
 
     super @WIDTH, @HEIGTH
     self.caption = "tuto jeux gosu"
+    @img = Gosu::Image.new("Gosu.jpeg", :tileable => true)
+    @background = Gosu::Image.new("M.jpg", :tileable => true)
+    #@sound = Gosu::Sound.new(,:tileable => true)
+    @x = 0
+    @y = 0
   end 
   
   def update
-    #logic
+    @x += 1
+    @y += 1
   end
 
   def draw
-    #
+    @img.draw(@x,@y,0)
   end
 end
 
